b41e12d54a6cb95b5307ceb5435a5fcb6b3977d1
Ian <iantirso.cini@gmail.com>
2024-02-25 22:45:56 +0100

    feat: wez term conf

---
+++ .wezterm.lua
@@ -0,0 +1,116 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+local act = wezterm.action
+
+config.automatically_reload_config = true
+
+config.font = wezterm.font 'Source Code Pro'
+config.color_scheme = 'Sonokai (Gogh)'
+config.font_size = 11.5
+
+config.scrollback_lines = 3500
+
+config.default_workspace = "home"
+
+config.window_decorations = "RESIZE"
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+config.tab_bar_at_bottom = true
+
+config.adjust_window_size_when_changing_font_size = true
+config.audible_bell = Disabled
+
+config.inactive_pane_hsb = {
+  -- saturation = 0.80,
+  brightness = 0.5
+}
+
+-- Keys
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+  -- Send C-a when pressing C-a twice
+  { key = "a",          mods = "LEADER|CTRL", action = act.SendKey { key = "a", mods = "CTRL" } },
+  { key = "c",          mods = "LEADER",      action = act.ActivateCopyMode },
+  { key = "phys:Space", mods = "LEADER",      action = act.ActivateCommandPalette },
+
+  -- Pane keybindings
+  { key = "s",          mods = "LEADER",      action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+  { key = "v",          mods = "LEADER",      action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+  { key = "h",          mods = "LEADER",      action = act.ActivatePaneDirection("Left") },
+  { key = "j",          mods = "LEADER",      action = act.ActivatePaneDirection("Down") },
+  { key = "k",          mods = "LEADER",      action = act.ActivatePaneDirection("Up") },
+  { key = "l",          mods = "LEADER",      action = act.ActivatePaneDirection("Right") },
+  { key = "q",          mods = "LEADER",      action = act.CloseCurrentPane { confirm = true } },
+  { key = "z",          mods = "LEADER",      action = act.TogglePaneZoomState },
+  { key = "o",          mods = "LEADER",      action = act.RotatePanes "Clockwise" },
+
+  { key = "r",          mods = "LEADER",      action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
+
+  -- Tab keybindings
+  { key = "t",          mods = "LEADER",      action = act.SpawnTab("CurrentPaneDomain") },
+  { key = "[",          mods = "LEADER",      action = act.ActivateTabRelative(-1) },
+  { key = "]",          mods = "LEADER",      action = act.ActivateTabRelative(1) },
+  { key = "n",          mods = "LEADER",      action = act.ShowTabNavigator },
+  {
+    key = "e",
+    mods = "LEADER",
+    action = act.PromptInputLine {
+      description = wezterm.format {
+        { Attribute = { Intensity = "Bold" } },
+        { Foreground = { AnsiColor = "Fuchsia" } },
+        { Text = "Renaming Tab Title...:" },
+      },
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end)
+    }
+  },
+  -- Key table for moving tabs around
+  { key = "m", mods = "LEADER",       action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
+  -- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
+  { key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
+  { key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
+
+  { key = "w", mods = "LEADER",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+
+}
+
+for i = 1, 9 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = act.ActivateTab(i - 1)
+  })
+end
+
+config.key_tables = {
+  resize_pane = {
+    { key = "h",      action = act.AdjustPaneSize { "Left", 1 } },
+    { key = "j",      action = act.AdjustPaneSize { "Down", 1 } },
+    { key = "k",      action = act.AdjustPaneSize { "Up", 1 } },
+    { key = "l",      action = act.AdjustPaneSize { "Right", 1 } },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter",  action = "PopKeyTable" },
+  },
+  move_tab = {
+    { key = "h",      action = act.MoveTabRelative(-1) },
+    { key = "j",      action = act.MoveTabRelative(-1) },
+    { key = "k",      action = act.MoveTabRelative(1) },
+    { key = "l",      action = act.MoveTabRelative(1) },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter",  action = "PopKeyTable" },
+  }
+}
+
+config.use_fancy_tab_bar = false
+config.status_update_interval = 1000
+return config
+
